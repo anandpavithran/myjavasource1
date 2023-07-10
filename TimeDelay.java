e1404cf89f28790ce8a02ae6532e8403ec7ca3c4
anandpavithran <58514903+anandpavithran@users.noreply.github.com>
2023-07-10 13:03:15 +0530

    Update TimeDelay.java

--- TimeDelay.java
+++ TimeDelay.java
@@ -2,7 +2,7 @@ public class TimeDelay{
   public static void main(String args[]) {
     try {
       while (true) {
-        System.out.println(new String("Hello world"));
+        System.out.println(new String("Hello world-10072023"));
         Thread.sleep(3 * 1000); // every 3 seconds
       }
     } catch (InterruptedException e) {
